4ec2541650ea23d2bf5d172be3400cf29ea63054
Siri Høyås <siri@skjervold.me>
2022-09-23 13:47:30 +0200

    Changed the tailwind cofig file with the correct content. addded the stylesheet link to all html pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./input.css"],
+  content: ["./*.{html,js}"],
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
-      xl: "1440px"
+      xl: "1440px",
     },
     extend: {},
   },
   plugins: [],
-}
+};
