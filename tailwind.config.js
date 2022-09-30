13d4bec1d7c408d0765d0217e9d79d35ea21d51f
Siri Høyås <siri@skjervold.me>
2022-09-30 21:34:01 +0200

    finished navbar for mobile and web

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,14 @@ module.exports = {
     extend: {
       backgroundImage: {
         profileBackgroundImage: (theme) => ({
-          "profileCard" : "url('https://images.unsplash.com/photo-1463194537334-3940784aa69a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
-        })
+          profileCard: "url('https://images.unsplash.com/photo-1463194537334-3940784aa69a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
+        }),
       },
       colors: {
-        fontWhite: 'hsl(0, 100%, 97%)',
-        filterBlue: 'hsl(216, 41%, 39%)',
-        mainBlue: 'hsl(216, 41%, 39%)'
+        fontWhite: "hsl(0, 100%, 97%)",
+        filterBlue: "hsl(216, 41%, 39%)",
+        mainBlue: "hsl(216, 41%, 39%)",
+        mainGray: "#D5D5D6",
       },
     },
   },
