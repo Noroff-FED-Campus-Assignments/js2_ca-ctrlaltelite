0089eb46a6c300523d97c2da06795712aeedc8fa
Siri Høyås <siri@skjervold.me>
2022-10-10 14:55:50 +0200

    made base function for adding comments. Need to implement it on the feed.html site! Also started to splitting my code into modules git push clear

---
+++ js/components/createPosts.mjs
@@ -0,0 +1,22 @@
+export function createPosts(container, post, userAvatar, userName) {
+  const html = (container.innerHTML += `
+    <div class="userPost flex flex-col border mb-5 rounded-sm p-2">
+    <div class="flex">
+        <img src="${userAvatar}" onerror="this.src = '/img/userPlacegolder.png';" class="w-8 h-8 rounded-full">
+        <p class="ml-2">${userName}</p>
+    </div>
+    
+    <div class="mb-4 mt-3">
+        <h2 class="">${post.title}<h2>
+        <img src="${post.media}">
+        <p>${post.body}</p>
+    </div>
+    <div>
+        <p class="mb-4">Reactions</p>
+
+</div>
+    
+    `);
+
+  return html;
+}
