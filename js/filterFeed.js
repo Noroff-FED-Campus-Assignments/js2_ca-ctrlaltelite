13f17436a3afa14a59b431f40e48a544a7bb5a36
Siri Høyås <siri@skjervold.me>
2022-10-13 11:28:06 +0200

    added function to feed to filter posts by reaction count

--- js/filterFeed.js
+++ js/filterFeed.js
@@ -1,9 +1,13 @@
 import { createPosts } from "./components/createPosts.mjs";
+import { getLocalStorage } from "./components/getLocalStorage.mjs";
+
+const { accessToken, userName } = getLocalStorage();
 
 const popularPostsBtn = document.querySelector(".popular");
 const feedBtn = document.querySelector(".feed");
 
 popularPostsBtn.addEventListener("click", filterByCommentCount);
+const container = document.querySelector("#post-section");
 
 async function filterByCommentCount() {
   popularPostsBtn.classList.add("text-mainGray");
@@ -11,7 +15,6 @@ async function filterByCommentCount() {
   feedBtn.classList.remove("text-mainGray");
   feedBtn.classList.add("text-mainBlue");
 
-  const container = document.querySelector("#post-section");
   container.innerHTML = "";
   const response = await fetch("https://nf-api.onrender.com/api/v1/social/posts?limit=300&comments=true&_author=true", {
     method: "GET",
@@ -34,4 +37,22 @@ async function filterByCommentCount() {
   }
 }
 
-feedBtn.addEventListener("click", getFeedPosts());
+feedBtn.addEventListener("click", getFeedPosts);
+
+async function getFeedPosts() {
+  const response = await fetch("https://nf-api.onrender.com/api/v1/social/posts/?_author=true&_reactions=true&_comments=true", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const json = await response.json();
+
+  container.innerHTML = "";
+  for (let i = 0; i < json.length; i++) {
+    console.log(json[i]);
+
+    createPosts(container, json[i], json[i].author.avatar, json[i].author.name);
+  }
+}
